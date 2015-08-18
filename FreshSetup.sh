d977e45f6d76da309ed671fde113e604cdd4a3a7
cadazab <cadazab@gmail.com>
2015-08-18 05:59:07 -0400

    github pasword caching

--- FreshSetup.sh
+++ FreshSetup.sh
@@ -10,3 +10,7 @@ apt-get install -y build-essential
 #Octave
 sudo apt-get install -y octave gnuplot
 
+#github pasword caching
+git config --global credential.helper cache #Set git to use the credential memory cache
+git config --global credential.helper 'cache --timeout=3600' #Set the cache to timeout after 1 hour (setting is in seconds)
+
