1e9563e792d5c067cfedb96dcd91fc3fb563936d
cadazab <cadazab@gmail.com>
2015-09-22 15:13:49 +0200

    Update FreshSetup.sh

--- FreshSetup.sh
+++ FreshSetup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#sudo add-apt-repository ppa:webupd8team/atom
 #sudo apt-get -y upgrade
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo apt-get -y update
@@ -7,38 +6,33 @@ sudo apt-get -y update
 #GUI
 sudo apt-get install -y ubuntu-desktop --no-install-recommends
 sudo apt-get install -y gnome-session gnome-panel metacity nautilus gnome-terminal
-#gnome-settings-daemon
 
 #VNC
-#sudo apt-get install -y tightvncserver
 sudo apt-get install -y vnc4server
 
 #Node
-#curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
-#sudo apt-get install -y nodejs
-#apt-get install -y build-essential
+curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
+sudo apt-get install -y nodejs
+apt-get install -y build-essential
 
 #Octave
 sudo apt-get install -y octave gnuplot
 touch .octaverc
 echo "graphics_toolkit('gnuplot')" > .octaverc
 
-#Atom
-#sudo apt-get install -y atom
-
 #Sublime
 sudo apt-get install -y sublime-text
 
 #Chromium
-#sudo apt-get install -y chromium-browser
+sudo apt-get install -y chromium-browser
 
 #ipython
-#sudo apt-get install -y libzmq-dev
-#sudo apt-get install -y python-dev
-#wget https://bootstrap.pypa.io/get-pip.py
-#sudo python get-pip.py
-#rm get-pip.py
-#sudo pip install "ipython[notebook]"
+sudo apt-get install -y libzmq-dev
+sudo apt-get install -y python-dev
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+rm get-pip.py
+sudo pip install "ipython[notebook]"
 
 #oh-my-zsh
 sudo apt-get install -y zsh
