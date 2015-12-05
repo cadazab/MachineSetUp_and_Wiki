88ce6305c1279bb6eff040d12a3731b1d69b7cda
cadazab <cadazab@gmail.com>
2015-12-05 15:40:41 +0100

    Update FreshSetup.sh

--- FreshSetup.sh
+++ FreshSetup.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y git
 curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y g++
-apt-get install -y build-essential
+sudo apt-get install -y build-essential
 sudo npm install kerberos
 sudo npm install mongodb
 
