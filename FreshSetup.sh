e7b55cf8c1d1973b1a22b31096c24d98d17661b1
cadazab <cadazab@gmail.com>
2016-02-08 13:06:42 +0100

    Update FreshSetup.sh

--- FreshSetup.sh
+++ FreshSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
@@ -21,7 +21,7 @@ sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 
 #MongoDB
-sudo apt-get install -y mongodb-org
+#sudo apt-get install -y mongodb-org
 
 
 #Octave
